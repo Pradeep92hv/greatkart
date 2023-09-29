a1d85f868c2fbd6872c8d33264b96f7fd3bc163b
Pradeep92hv <hvpradeep92@gmail.com>
2023-09-30 00:07:03 +0530

    3rd folder completed

--- category/admin.py
+++ category/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Category
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'slug', 'discription', 'cat_image')
+    prepopulated_fields = {'slug': ('category_name',)}
+
+# Register the Category model with the CategoryAdmin class
+admin.site.register(Category, CategoryAdmin)
--- category/models.py
+++ category/models.py
@@ -4,14 +4,15 @@ from django.db import models
 
 class Category(models.Model):
     category_name=models.CharField(max_length=50,unique=True)
-    slug=models.CharField(max_length=500,unique=True)
+    slug=models.SlugField(max_length=500,unique=True)
     discription=models.TextField(max_length=300,blank=True)
-    cat_image=models.ImageField(upload_to='photos.category',blank=True)
+    cat_image=models.ImageField(upload_to='photos/category',blank=True)
     
     
     class Meta:
         verbose_name='category'
         verbose_name_plural='categories'
     
-    def __self__(self):
+    def __str__(self):
         return self.category_name
+
