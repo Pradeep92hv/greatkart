ba535d8affaab94a201e8004df887b30122bc601
Pradeep92hv <hvpradeep92@gmail.com>
2023-10-01 16:20:39 +0530

    8th folder completed

--- store/views.py
+++ store/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render,get_object_or_404
 
 from .models import Product
 from category.models import Category
+from carts.models import CartItem,Cart
 
+from carts.views import _cart_id
 def store(request,category_slug=None):
     categories=None
     products=None
@@ -14,6 +16,7 @@ def store(request,category_slug=None):
        categories= get_object_or_404(Category, slug=category_slug)
        products=Product.objects.filter(category=categories,is_available=True)
        products_count=products.count()
+       
     else:
         products=Product.objects.filter(is_available=True)
         products_count=products.count()
@@ -31,6 +34,9 @@ def product_details(request,category_slug,product_slug):
         # single_product=Product.objects.get(category__slug=category_slug,slug=product_slug)
         selected_category = get_object_or_404(Category, slug=category_slug)
         single_product = get_object_or_404(Product, category=selected_category, slug=product_slug)
+        
+        in_cart=CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()     # to check is product already in cart or not
+        
     except Exception as e:
         raise e
     
@@ -39,5 +45,7 @@ def product_details(request,category_slug,product_slug):
     #  only  above 2 line are working
     context={
         'single_product': single_product,
+        'in_cart':in_cart,
+        
     }    
     return render(request,'store/product_details.html',context)
