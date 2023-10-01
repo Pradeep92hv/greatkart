e660f320479ee2b60ccaa744499a296c9a9f4ba2
Pradeep92hv <hvpradeep92@gmail.com>
2023-10-01 17:58:24 +0530

    9th folder 3videos completed, basic paginator

--- store/views.py
+++ store/views.py
@@ -7,6 +7,35 @@ from category.models import Category
 from carts.models import CartItem,Cart
 
 from carts.views import _cart_id
+
+
+# def store(request,category_slug=None):
+#     categories=None
+#     products=None
+    
+#     if category_slug!=None:
+#        print(category_slug)
+#        categories= get_object_or_404(Category, slug=category_slug)
+#        products=Product.objects.filter(category=categories,is_available=True)
+#        products_count=products.count()
+       
+#     else:
+#         products=Product.objects.filter(is_available=True)
+#         products_count=products.count()
+        
+#     context={
+#         'products':products,
+#         'products_count':products_count,
+#     }
+#     return render(request,'store/store.html',context)
+
+
+#  for pagination
+
+
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
+
+
 def store(request,category_slug=None):
     categories=None
     products=None
@@ -16,13 +45,19 @@ def store(request,category_slug=None):
        categories= get_object_or_404(Category, slug=category_slug)
        products=Product.objects.filter(category=categories,is_available=True)
        products_count=products.count()
+       paginator=Paginator(products,4)
+       page=request.GET.get('page')
+       paged_products=paginator.get_page(page)
        
     else:
         products=Product.objects.filter(is_available=True)
+        paginator=Paginator(products,3)
+        page=request.GET.get('page')
+        paged_products=paginator.get_page(page)
         products_count=products.count()
         
     context={
-        'products':products,
+        'products':paged_products,
         'products_count':products_count,
     }
     return render(request,'store/store.html',context)
