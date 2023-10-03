7d24dd6733ecae2f47a243a4eb710259fb9b4831
Pradeep92hv <hvpradeep92@gmail.com>
2023-10-03 12:27:29 +0530

    10.2 folder product variation in admin

--- store/admin.py
+++ store/admin.py
@@ -3,12 +3,19 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import Product
+from .models import Product,variation
 class ProductAdmin(admin.ModelAdmin):
 # class ProductAdmin(models.ModelAdmin):
     list_display=('product_name','price','stock','category','modified','is_available')
     prepopulated_fields={'slug':('product_name',)}
     
 
+class variationAdmin(admin.ModelAdmin):
+    list_display=('product','variation_category','variation_value','is_active')
+    list_editable=('is_active',)
+    list_filter=('product','variation_category','variation_value','is_active')
+     
+
 
 admin.site.register(Product,ProductAdmin)
+admin.site.register(variation,variationAdmin)
